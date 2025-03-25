a53d823d42fee39dc815caff9a6c4ad79527d832
Fernando Morales <10418554@mackenzista.com.br>
2025-03-25 01:26:35 -0300

    Update WebConfig.java

--- conversor-moeda/src/main/java/com/example/conversor_moeda/config/WebConfig.java
+++ conversor-moeda/src/main/java/com/example/conversor_moeda/config/WebConfig.java
@@ -9,10 +9,13 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("http://44.222.138.241:8080");
+        // Permitir CORS apenas para a origem específica
+        registry.addMapping("/**")  // Permite CORS em todos os endpoints
+                .allowedOrigins("http://44.222.138.241:8080")  // Substitua pela URL do front-end
+                .allowedMethods("GET", "POST", "PUT", "DELETE")  // Métodos HTTP permitidos
+                .allowedHeaders("*")  // Permite todos os cabeçalhos
+                .allowCredentials(true);  // Permite enviar cookies, se necessário
     }
-    
 }
