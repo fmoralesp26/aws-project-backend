0c62fc378ca422b133a905975691580494b86b4f
Fernando Morales <10418554@mackenzista.com.br>
2025-03-25 00:51:01 -0300

    Update WebConfig.java

--- conversor-moeda/src/main/java/com/example/conversor_moeda/config/WebConfig.java
+++ conversor-moeda/src/main/java/com/example/conversor_moeda/config/WebConfig.java
@@ -1,18 +1,17 @@
-package com.example.conversor_moeda.config;
-
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
-@EnableWebMvc
 public class WebConfig implements WebMvcConfigurer {
 
-    @SuppressWarnings("null")
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("http://44.222.138.241:8080/");
+        registry.addMapping("/**")
+                .allowedOrigins("http://44.204.219.241:8080") // URL do seu front-end
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*")
+                .allowCredentials(true);
     }
-    
 }
