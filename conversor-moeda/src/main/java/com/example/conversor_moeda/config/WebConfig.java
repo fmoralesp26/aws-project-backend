e0ff72fc8217bc596f4ef047bb2ee11e90524102
Fernando Morales <10418554@mackenzista.com.br>
2025-03-24 22:51:58 -0300

    Add files via upload

---
+++ conversor-moeda/src/main/java/com/example/conversor_moeda/config/WebConfig.java
@@ -0,0 +1,18 @@
+package com.example.conversor_moeda.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+@EnableWebMvc
+public class WebConfig implements WebMvcConfigurer {
+
+    @SuppressWarnings("null")
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+    
+}
