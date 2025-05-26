99e1244f4ff1d3d0575f2a0e2c592191c611880e
Fernando Morales <10418554@mackenzista.com.br>
2025-05-26 19:38:29 -0300

    Delete conversor-moeda directory

--- conversor-moeda/src/main/java/com/example/conversor_moeda/moeda/MoedaRepo.java
+++
@@ -1,9 +0,0 @@
-package com.example.conversor_moeda.moeda;
-
-import java.util.Optional;
-
-import org.springframework.data.repository.CrudRepository;
-
-public interface MoedaRepo extends CrudRepository<Moeda, Long> {
-    Optional<Moeda> findByPaisIgnoreCase(String pais);
-}
