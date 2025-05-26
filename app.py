aef42b6ff4913c0df1e781ffd717b100e4740dbc
Fernando Morales <10418554@mackenzista.com.br>
2025-05-26 19:41:19 -0300

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
-from flask import Flask, request, jsonify
+from flask import Flask
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)
 
 class Moeda:
     def __init__(self, nome, valor):
